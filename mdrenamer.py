6f19c5d321705761e82bf0504bd4a14c17a2cafd
Chad Zawistowski <chad.zawistowski@gmail.com>
2014-02-21 15:54:57 -0500

    Complete overhaul

--- mdrenamer.py
+++ mdrenamer.py
@@ -27,117 +27,168 @@
 import os
 import sys
 import struct
+import argparse
 
-def parseArguments(arguments):
-	inputPath = arguments[0]
-	modName = arguments[1]
-	shortName = arguments[2]
-	buildNumberString = arguments[3]
-	outputDirectory = arguments[4] if len(arguments) >= 5 else os.path.dirname(inputPath)
+def md_rename_file(input_map, output_path, new_filename, mod_name):
+    with open(input_map, 'rb') as input_file:
+        map_buffer = bytearray(input_file.read())
 
-	if not os.path.exists(inputPath):
-		raise Exception("%s does not exist" % inputPath)
+    def has_bytes(offset, bytes):
+        """Returns true if the bytes at the offset match our bytestring."""
+        return map_buffer[offset:offset + len(bytes)] == bytes
 
-	if not inputPath.endswith(".map"):
-		raise Exception("%s is not a .map file" % inputPath)
+    def read_bytes(offset, length):
+        """Reads <length> bytes from the specified offset."""
+        return map_buffer[offset:offset + length]
 
-	if os.path.isdir(inputPath):
-		raise Exception("%s is a directory.. which is not good" % inputPath)
+    def write_bytes(offset, data):
+        """Writes bytes to the specified offset."""
+        map_buffer[offset:offset + len(data)] = data
 
-	try:
-		buildNumber = int(buildNumberString)
-	except ValueError:
-		raise Exception("Build number is not a valid number")
+    def clear_and_write_bytes(offset, num_zeroes, data):
+        """Writes a number of null bytes, then writes data to the specified offset."""
+        write_bytes(offset, b'\x00' * num_zeroes)
+        write_bytes(offset, data)
 
-	if buildNumber <= 0:
-		raise Exception("Build number is too small")
+    def read_uint32(offset):
+        """Reads a little-endian int32 from the specified offset."""
+        return struct.unpack('<I', read_bytes(offset, 0x4))[0]
 
-	if len(shortName) == 0:
-		raise Exception("Short name cannot be empty")
+    def filter_tags_by_name(index_entry_offsets, name, skip_ustr=True):
+        """Yields each tag which has the specified name. Optionally skips ustr tags."""
 
-	if len(modName) > 13:
-		raise Exception("Mod name must be less than 13 characters")
+        for curr_offset in index_entry_offsets:
+            if skip_ustr:
+                # skipping tags of class ustr
+                if read_bytes(curr_offset, 0x4) != b'rtsu':
+                    continue
 
-	if len(modName) == 0:
-		raise Exception("Mod name must be greater than 0 characters")
+            name_offset = read_uint32(curr_offset + 0x10) - map_magic
+            if has_bytes(name_offset, name):
+                yield curr_offset
 
-	mapFileName = "%s_%d" % (shortName, buildNumber)
-	if len(mapFileName) > 13:
-		raise Exception("Map name is too long. Try shortening the short name")
+    def update_map_names(offset):
+        """Given """
+        map_names_offset = read_uint32(offset + 0x14) - map_magic + 0x1B0
 
-	if mapFileName.lower() != mapFileName:
-		raise Exception("Map name has capital letter. The short name must be all lowercase")
+        def replace_wide_name(offset, old_name, new_name):
+            """Replaces a utf16 name with another utf16 string"""
+            clear_and_write_bytes(offset, (len(old_name) + 1) * 2, new_name.encode('utf-16le'))
 
-	outputPath = os.path.join(outputDirectory, mapFileName + ".map")
-	if os.path.exists(outputPath):
-		raise Exception("Map already exists at %s ..aborting for safety.." % outputPath)
+        replace_wide_name(map_names_offset + 0x48,  "Rat Race",         "Random")
+        replace_wide_name(map_names_offset + 0xB4,  "Boarding Action",  "Barrier")
+        replace_wide_name(map_names_offset + 0xD4,  "Blood Gulch",      "Blood Gulch")
+        replace_wide_name(map_names_offset + 0x174, "Infinity",         "Crossing")
+        replace_wide_name(map_names_offset + 0x1A0, "Gephyrophobia",    mod_name)
 
-	return inputPath, outputPath, mapFileName, modName
+    def update_map_descriptions(offset):
+        """Given """
+        map_descr_offset = read_uint32(offset + 0x14) - map_magic + 0x19C
 
-def mdRenameFile(arguments):
-	inputPath, outputPath, mapFileName, modName = parseArguments(arguments)
-	with open(inputPath, "rb") as inputFile:
-		mapBuffer = bytearray(inputFile.read())
+        originalBloodgulchBytes = bytes([
+            0x54, 0x00, 0x68, 0x00, 0x65, 0x00, 0x20, 0x00, 0x51, 0x00, 0x75, 0x00, 0x69, 0x00, 0x63, 0x00,
+            0x6B, 0x00, 0x20, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x61, 0x00, 0x6E, 0x00, 0x64, 0x00, 0x20, 0x00,
+            0x74, 0x00, 0x68, 0x00, 0x65, 0x00, 0x20, 0x00, 0x44, 0x00, 0x65, 0x00, 0x61, 0x00, 0x64, 0x00,
+            0x0D, 0x00, 0x0A, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x34, 0x00, 0x2D, 0x00, 0x31, 0x00, 0x36, 0x00,
+            0x20, 0x00, 0x70, 0x00, 0x6C, 0x00, 0x61, 0x00, 0x79, 0x00, 0x65, 0x00, 0x72, 0x00, 0x73, 0x00,
+            0x0D, 0x00, 0x0A, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x53, 0x00, 0x75, 0x00, 0x70, 0x00, 0x70, 0x00,
+            0x6F, 0x00, 0x72, 0x00, 0x74, 0x00, 0x73, 0x00, 0x20, 0x00, 0x76, 0x00, 0x65, 0x00, 0x68, 0x00,
+            0x69, 0x00, 0x63, 0x00, 0x6C, 0x00, 0x65, 0x00, 0x73, 0x00])
 
-	readBytes = lambda offset, length: mapBuffer[offset:offset + length]
-	readUInt32Little = lambda offset: struct.unpack('<I', readBytes(offset, 0x4))[0]
-	hasBytes = lambda offset, bytes: mapBuffer[offset:offset + len(bytes)] == bytes
-	def writeBytes(offset, bytes, zeroPaddingLength=0):
-		mapBuffer[offset:offset + zeroPaddingLength] = b"\x00" * zeroPaddingLength
-		mapBuffer[offset:offset + len(bytes)] = bytes
+        clear_and_write_bytes(map_descr_offset + 0x12C, 0x56,   "Where would you\r\nlike to go?".encode("utf-16le"))
+        clear_and_write_bytes(map_descr_offset + 0x2BC, 0x48,   "So Close,\r\nYet So Far..".encode("utf-16le"))
+        write_bytes(          map_descr_offset + 0x308,         originalBloodgulchBytes)
+        clear_and_write_bytes(map_descr_offset + 0x634, 0x86,   "A Memorial to\r\nHeroes Fallen".encode("utf-16le"))
+        clear_and_write_bytes(map_descr_offset + 0x740, 0x64,   "Modded".encode("utf-16le"))
 
-	if readUInt32Little(0x4) != 7:
-		raise Exception("This is not a valid (version 7) full-version map!")
 
-	writeBytes(0x20, mapFileName.encode("utf-8"), 0xF)
+    ## Do the work!
 
-	indexOffset = readUInt32Little(0x10)
-	magic = readUInt32Little(indexOffset) - indexOffset - 0x28
-	numberOfTags = readUInt32Little(indexOffset + 0xC)
-	tagArrayOffset = indexOffset + 0x28
+    # process the map header
+    game_version = read_uint32(0x4)
+    index_header_offset = read_uint32(0x10)
 
-	for tagIndex in range(numberOfTags):
-		currentLocation = 0x20 * tagIndex + tagArrayOffset
-		if readBytes(currentLocation, 0x4) != b"rtsu":
-			continue
+    if game_version != 7:
+        raise Exception('This is not a valid (version 7) full-version map!')
 
-		tagOffset = readUInt32Little(currentLocation + 0x10) - magic
+    # process the index header
+    index_offset = read_uint32(index_header_offset)
+    tag_count = read_uint32(index_header_offset + 0xC)
+    index_entry_offsets = (index_offset + 0x20 * i for i in range(tag_count))
 
-		if hasBytes(tagOffset, b"ui\\shell\\main_menu\\mp_map_list"):
-			namesOffset = readUInt32Little(currentLocation + 0x14) - magic + 0x1B0
+    map_magic = index_offset - index_header_offset
 
-			writeBytes(namesOffset + 0x48, "Random".encode("utf-16le"), (len("Rat Race") + 1) * 2)
-			writeBytes(namesOffset + 0xB4, "Barrier".encode("utf-16le"), (len("Boarding Action") + 1) * 2)
-			writeBytes(namesOffset + 0xD4, "Blood Gulch".encode("utf-16le"), (len("Blood Gulch") + 1) * 2)
-			writeBytes(namesOffset + 0x174, "Crossing".encode("utf-16le"), (len("Infinity") + 1) * 2)
-			writeBytes(namesOffset + 0x1A0, modName.encode("utf-16le"), (len("Gephyrophobia") + 1) * 2)
+    # write the new internal name
+    clear_and_write_bytes(0x20, 0xF, new_filename.encode('ascii'))
 
-		elif hasBytes(tagOffset, b"ui\\shell\\main_menu\\multiplayer_type_select\\mp_map_select\\map_data"):
-			descriptionOffset = readUInt32Little(currentLocation + 0x14) - magic + 0x19C
-			originalBloodgulchBytes = bytes([0x54, 0x00, 0x68, 0x00, 0x65, 0x00, 0x20, 0x00, 0x51, 0x00, 0x75, 0x00, 0x69, 0x00, 0x63, 0x00, 0x6B, 0x00, 0x20, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x61, 0x00, 0x6E, 0x00, 0x64, 0x00, 0x20, 0x00, 0x74, 0x00, 0x68, 0x00, 0x65, 0x00, 0x20, 0x00, 0x44, 0x00, 0x65, 0x00, 0x61, 0x00, 0x64, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x34, 0x00, 0x2D, 0x00, 0x31, 0x00, 0x36, 0x00, 0x20, 0x00, 0x70, 0x00, 0x6C, 0x00, 0x61, 0x00, 0x79, 0x00, 0x65, 0x00, 0x72, 0x00, 0x73, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x0D, 0x00, 0x0A, 0x00, 0x53, 0x00, 0x75, 0x00, 0x70, 0x00, 0x70, 0x00, 0x6F, 0x00, 0x72, 0x00, 0x74, 0x00, 0x73, 0x00, 0x20, 0x00, 0x76, 0x00, 0x65, 0x00, 0x68, 0x00, 0x69, 0x00, 0x63, 0x00, 0x6C, 0x00, 0x65, 0x00, 0x73, 0x00])
+    # update multiplayer map names
+    for tag in filter_tags_by_name(index_entry_offsets, b'ui\\shell\\main_menu\\mp_map_list'):
+        update_map_names(tag)
 
-			writeBytes(descriptionOffset + 0x12C, "Where would you\r\nlike to go?".encode("utf-16le"), 0x56)
-			writeBytes(descriptionOffset + 0x2BC, "So Close,\r\nYet So Far..".encode("utf-16le"), 0x48)
-			writeBytes(descriptionOffset + 0x308, originalBloodgulchBytes)
-			writeBytes(descriptionOffset + 0x634, "A Memorial to\r\nHeroes Fallen".encode("utf-16le"), 0x86)
-			writeBytes(descriptionOffset + 0x740, "Modded".encode("utf-16le"), 0x64)
+    # update multiplayer map descriptions
+    for tag in filter_tags_by_name(index_entry_offsets, b'ui\\shell\\main_menu\\multiplayer_type_select\\mp_map_select\\map_data'):
+        update_map_descriptions(tag)
 
-	with open(outputPath, "wb") as outputFile:
-		outputFile.write(mapBuffer)
+    # save under a new filename
+    with open(output_path, "wb") as outputFile:
+        outputFile.write(map_buffer)
 
-	return outputPath
 
 if __name__ == '__main__':
-	if len(sys.argv[1:]) == 0:
-		print("Usage: %s <input_map> <mod_name> <short_name> <build_number> [output_directory]\n" % sys.argv[0])
-		print("Example: %s ~/Desktop/Maps/bloodgulch.map \"Best Mod\" bestmod 1\n" % sys.argv[0])
-		print("Arguments:\n")
-		print("<input_map> The file path to the Halo map file to rename\n")
-		print("<mod_name> The user friendly name of the mod. Must be <= 13 characters; truncate if necessary. This is not the same as the name that shows up in the MD database, but the one that shows in the map selection menu in-game.\n")
-		print("<short_name> The internal name of the mod that will be used in the outputting filename. Must be all lowercase and >= 1 character(s)\n")
-		print("<build_number> The build number of the mod which is also used in the outputting filename. This should be increased before any sort of public or private distribution. Must be an integer >= 1\n")
-		print("[output_directory] The directory to output the new MD renamed map in. This is optional and defaults to the directory <input_map> is in\n")
-		print("Additionally, the following constraint must be met:")
-		print("string_length(<short_name>) + string_length(<build_number>) < 13")
-	else:
-		mdRenameFile(sys.argv[1:])
+    parser = argparse.ArgumentParser(description='Converts a map to MD format by updating the internal map names.')
+    parser.add_argument('input_map', help='Path to the Halo mapfile to rename.')
+    parser.add_argument('mod_name', help='User friendly name of the mod. Must be <= 13 characters. This is the name that shows in the map selection menu in-game.')
+    parser.add_argument('short_name', help='Internal name of the mod, and the new filename. Must be all lowercase and >= 1 character(s).')
+    parser.add_argument('build_number', help='The build number of the mod which is also used in the outputting filename. This should be increased before any sort of public or private distribution. Must be an integer >= 1.')
+    parser.add_argument('output_directory', nargs='?', help='Directory to output the new MD renamed map in. This is optional and defaults to the directory <input_map> is in.')
+    args = parser.parse_args()
+
+    input_map = args.input_map
+    mod_name = args.mod_name
+    short_name = args.short_name
+    build_number = args.build_number
+    output_directory = args.output_directory
+
+    ## validate input
+
+    if output_directory == None:
+        output_directory = os.path.dirname(input_map)
+
+    if not os.path.exists(input_map):
+        raise Exception("%s does not exist" % input_map)
+
+    if not input_map.endswith(".map"):
+        raise Exception("%s is not a .map file" % input_map)
+
+    if os.path.isdir(input_map):
+        raise Exception("%s is a directory.. which is not good" % input_map)
+
+    try:
+        buildNumber = int(build_number)
+    except ValueError:
+        raise Exception("Build number is not a valid number")
+
+    if buildNumber <= 0:
+        raise Exception("Build number is too small")
+
+    if len(short_name) == 0:
+        raise Exception("Short name cannot be empty")
+
+    if len(mod_name) > 13:
+        raise Exception("Mod name must be less than 13 characters")
+
+    if len(mod_name) == 0:
+        raise Exception("Mod name must be greater than 0 characters")
+
+    new_filename = "%s_%d" % (short_name, buildNumber)
+    if len(new_filename) > 13:
+        raise Exception("Map name '%s' is too long. Try shortening the short name" % new_filename)
+
+    if new_filename.lower() != new_filename:
+        raise Exception("Map name '%s' has capital letter. The short name must be all lowercase" % new_filename)
+
+    output_path = os.path.join(output_directory, new_filename + ".map")
+    if os.path.exists(output_path):
+        raise Exception("Map already exists at %s ..aborting for safety.." % output_path)
+
+    md_rename_file(input_map, output_path, new_filename, mod_name)
